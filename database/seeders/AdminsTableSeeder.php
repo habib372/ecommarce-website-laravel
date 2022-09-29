0cff6444e3ff1d21bdb775ac1fa2e79f18f1da98
habib372 <habibsoft372@gmail.com>
2022-09-29 23:32:01 +0600

    seeder update

--- database/seeders/AdminsTableSeeder.php
+++ database/seeders/AdminsTableSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use database\factories\AdminsFactory;
 use App\Models\Admin;
+use Illuminate\Support\Str;
 use Hash;
 use DB;
 
@@ -18,7 +20,11 @@ class AdminsTableSeeder extends Seeder
     {
         DB::table('admins')->delete();
         Admin::create([
-            'id'=>1, 'name'=>'admin', 'type'=>'admin', 'mobile'=>'01767177372', 'email'=>'habib@gmail.com', 'email_verified_at'=>'2022-05-07 03:25:10', 'password'=>Hash::make('123456'), 'image'=>'habib.jpg', 'status'=>'1', 'remember_token'=>'', 'created_at'=>'2020-01-01', 'updated_at'=>'2022-01-01'
+            'id'=>1, 'name'=>'admin', 'type'=>'admin', 'mobile'=>'01767177372', 'email'=>'habib@gmail.com', 'email_verified_at'=>'2022-05-07 03:25:10', 'password'=>Hash::make('123456'), 'image'=>'habib.jpg', 'status'=>'1', 'remember_token' => Str::random(10), 'created_at'=>'2020-01-01', 'updated_at'=>'2022-01-01'
+        ],
+        [
+            'id'=>2, 'name'=>'User ', 'type'=>'User', 'mobile'=>'01767135214', 'email'=>'User@gmail.com', 'email_verified_at'=>now(), 'password'=>Hash::make('654321'), 'image'=>'user.jpg', 'status'=>'1', 'remember_token' => Str::random(10), 'created_at'=>'2020-01-01', 'updated_at'=>'2022-01-01'
         ]);
+    
     }
 }
