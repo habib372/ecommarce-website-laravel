c363a094e2612c9dfc34ecece11abd04002d7fa1
habib372 <habibsoft372@gmail.com>
2022-10-02 03:02:27 +0600

    update

--- resources/views/admin/admin_login.blade.php
+++ resources/views/admin/admin_login.blade.php
@@ -24,14 +24,14 @@
     <div class="card-body">
       <p class="login-box-msg">Sign in to start your session</p>
       
-      <!-- @if(Session::has('error_msg'))
+      @if(Session::has('error_msg'))
         <div class="alert alert-danger alert-dismissible fade show" role="alert">
           {{ Session::get('error_msg') }}
           <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-      @endif -->
+      @endif
 
       @if ($errors->any())
         <div class="alert alert-danger">
