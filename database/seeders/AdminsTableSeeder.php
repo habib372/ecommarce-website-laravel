2a133b59e56c98ddd550d0075d7c25a32fc5ced8
Habibur Rahman (habib) <54948423+habib372@users.noreply.github.com>
2022-10-02 14:18:53 +0600

    Update AdminsTableSeeder.php

--- database/seeders/AdminsTableSeeder.php
+++ database/seeders/AdminsTableSeeder.php
@@ -24,11 +24,14 @@ class AdminsTableSeeder extends Seeder
 
         DB::table('admins')->insert([                                         //2. multiple data insert
             [
-                'id'=>1, 'name'=>'admin', 'type'=>'admin', 'mobile'=>'01767177372', 'email'=>'habibsoft372@gmail.com', 'email_verified_at'=>'2022-05-07 03:25:10', 'password'=>Hash::make('123456'), 'image'=>'habib.jpg', 'status'=>'1', 'remember_token' => Str::random(10), 'created_at'=>'2020-01-01', 'updated_at'=>'2022-01-01'
+                'id'=>1, 'name'=>'admin', 'type'=>'superadmin', 'mobile'=>'01767177372', 'email'=>'habibsoft372@gmail.com', 'email_verified_at'=>'2022-05-07 03:25:10', 'password'=>Hash::make('123456'), 'image'=>'superadmin.jpg', 'status'=>'1', 'remember_token' => Str::random(10), 'created_at'=>'2020-01-01', 'updated_at'=>'2020-05-01'
             ],
             [
-                'id'=>2, 'name'=>'Users ', 'type'=>'User', 'mobile'=>'01767135214', 'email'=>'User@gmail.com', 'email_verified_at'=>now(), 'password'=>Hash::make('654321'), 'image'=>'user.jpg', 'status'=>'2', 'remember_token' => Str::random(10), 'created_at'=>'2020-01-01', 'updated_at'=>'2022-01-01'
-            ],      
+                'id'=>2, 'name'=>'User1 ', 'type'=>'subadmin', 'mobile'=>'01767135214', 'email'=>'subadmin@gmail.com', 'email_verified_at'=>now(), 'password'=>Hash::make('123'), 'image'=>'subadmin.jpg', 'status'=>'2', 'remember_token' => Str::random(10), 'created_at'=>'2021-01-01', 'updated_at'=>'2021-06-01'
+            ],
+            [
+                'id'=>3, 'name'=>'User2 ', 'type'=>'User', 'mobile'=>'01767325485', 'email'=>'User@gmail.com', 'email_verified_at'=>now(), 'password'=>Hash::make('111'), 'image'=>'user.jpg', 'status'=>'3', 'remember_token' => Str::random(10), 'created_at'=>'2022-05-01', 'updated_at'=>'2022-03-01'
+            ],     
         ]);
 
     }
